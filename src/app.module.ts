4b786dec849dbffe87cb44f6776e291d0ef94297
babalakk <babalakk@gmail.com>
2022-07-06 22:20:49 +0800

    fix typeorm import error

--- src/app.module.ts
+++ src/app.module.ts
@@ -41,7 +41,7 @@ function get_db_options() {
           rejectUnauthorized: false,
         },
       },
-      entities: [__dirname + '/../build/**/*.entity.js'],
+      entities: ['**/*.entity.js'],
     };
     Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
   } else {
