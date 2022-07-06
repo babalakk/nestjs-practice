62de30bbd12f73342d5b45193d37ce6922146632
babalakk <babalakk@gmail.com>
2022-07-06 17:41:58 +0800

    fix mail library bug

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { NodeMailgun } from 'ts-mailgun';
 import * as dotenv from 'dotenv';
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
 
 @Injectable()
 export class MailService {
-  mailer: NodeMailgun;
+  mg: any;
 
   constructor() {
     dotenv.config();
-    const mailer = new NodeMailgun();
-    mailer.apiKey = process.env.MAIL_GUN_API_KEY;
-    mailer.domain = process.env.MAIL_GUN_DOMAIN;
-    mailer.fromEmail = 'noreply@my-sample-app.com';
-    mailer.fromTitle = 'My Sample App';
-    mailer.init();
-    this.mailer = mailer;
+    const mg = mailgun.client({username: 'api', key: process.env.MAIL_GUN_API_KEY });
+    this.mg = mg;
   }
 
   async send(to: string, content: string) {
-    this.mailer
-      .send(to, 'Hello!', content)
-      .then((result) => console.log('Done', result))
-      .catch((error) => console.error('Error: ', error));
+    this.mg.messages.create(process.env.MAIL_GUN_DOMAIN, {
+      from: "noreply <noreply@my-sample-app.com'>",
+      to: [to],
+      subject: "Email from aha-exam program",
+      text: content,
+    })
+    .then(msg => console.log(msg)) // logs response data
+    .catch(err => console.error(err)); // logs any error
   }
 }
