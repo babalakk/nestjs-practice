059640aacfcdeca46eae0b0355ce7ddb8934d738
babalakk <babalakk@gmail.com>
2022-07-06 22:11:56 +0800

    rm unused files

--- src/social/social.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SocialMiddleware } from './social.middleware';
-
-describe('SocialMiddleware', () => {
-  it('should be defined', () => {
-    expect(new SocialMiddleware()).toBeDefined();
-  });
-});
